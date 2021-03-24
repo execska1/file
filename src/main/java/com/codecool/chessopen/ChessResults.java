3ea234ae5f859c935cc98573e9d19c834827562a
execska1 <execska@gmail.com>
2021-03-24 15:44:48 +0100

    Implement methods in ChessResults

--- src/main/java/com/codecool/chessopen/ChessResults.java
+++ src/main/java/com/codecool/chessopen/ChessResults.java
@@ -1,20 +1,39 @@
 package com.codecool.chessopen;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.io.*;
+import java.util.*;
 
 public class ChessResults {
 
     public List<String> getCompetitorsNamesFromFile(String fileName){
-        //TODO your code comes here
-        return null;
+        List<String> result = new ArrayList<>();
+        HashMap<String, Integer> players = collectNamesAndPointsFromFile(fileName);
+
+        players.entrySet().stream().sorted(Map.Entry.comparingByValue(Comparator.reverseOrder()))
+                .forEach(entry -> result.add(entry.getKey()));
+        return result;
+    }
+
+    private HashMap<String, Integer> collectNamesAndPointsFromFile(String fileName) {
+        HashMap<String, Integer> result = new HashMap<>();
+
+        try (Scanner scanner = new Scanner(new File(fileName))) {
+            while (scanner.hasNext()) {
+                Scanner line = new Scanner(scanner.nextLine());
+                line.useDelimiter(",");
+                String name = line.next();
+                int points = 0;
+
+                while (line.hasNext()) {
+                    points += line.nextInt();
+                }
+
+                result.put(name, points);
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found!");
+        }
+        return result;
     }
 
 }
